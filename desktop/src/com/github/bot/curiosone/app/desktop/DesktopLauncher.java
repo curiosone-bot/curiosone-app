dbe370f8f5f2183f44fc8bc589220b77c067184a
simonepri <simone.pri@hotmail.it>
2017-07-16 18:37:05 +0200

    Initial setup

---
+++ desktop/src/com/github/bot/curiosone/app/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.bot.curiosone.app.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.bot.curiosone.app.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
