d42ec99648d83d4089eebdc4adea9cc472e327fa
Emanuele Volanti <emavolant@gmail.com>
2017-07-19 12:40:19 +0200

    delete screens dir

--- core/src/com/github/bot/curiosone/app/workflow/Chat.java
+++ core/src/com/github/bot/curiosone/app/workflow/Chat.java
@@ -2,11 +2,7 @@ package com.github.bot.curiosone.app.workflow;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-<<<<<<< HEAD
 import com.github.bot.curiosone.app.workflow.chatscreens.ChatScreen;
-=======
-import com.github.bot.curiosone.app.workflow.screens.ChatScreen;
->>>>>>> master
 
 public class Chat extends Game {
 
@@ -19,15 +15,8 @@ public class Chat extends Game {
   @Override
   public void create() {
     batch = new SpriteBatch();
-<<<<<<< HEAD
     setScreen(new ChatScreen(this));
-=======
-<<<<<<< HEAD
     setScreen(new ChatScreen(this));
-=======
-    
->>>>>>> 4cc0d37a460a88e299f55e9153bf51c32449de4f
->>>>>>> master
   }
 
   @Override
