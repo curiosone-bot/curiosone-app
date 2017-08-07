a37bc781dccc2899a0ee023d510886e9686f2589
neker97 <officialnekerinf@gmail.com>
2017-08-07 19:28:11 +0200

    switch avaiable

--- core/src/com/github/bot/curiosone/app/chat/world/AITemporanea.java
+++ core/src/com/github/bot/curiosone/app/chat/world/AITemporanea.java
@@ -19,7 +19,7 @@ public class AITemporanea {
         answer = "bene grazie";
         break;
       case "che cos'e' il super sayan blu?":
-        answer = "e' il super sayan di un sayan che ha superato la potenza del super sayan go diventando super sayan god super sayan";
+        answer = "e' il super sayan di un sayan che ha superato la potenza del super sayan god diventando super sayan god super sayan";
         break;
       case "a che ora?":
         answer = "alle 9 in piazza";
