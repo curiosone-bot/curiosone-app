33fc25288b5896385840ffe403656c783b513e4e
Emanuele Volanti <emavolant@gmail.com>
2017-08-10 18:02:56 +0200

    Create link between SendButton and world

--- core/src/com/github/bot/curiosone/app/chat/chatObjs/SendButton.java
+++ core/src/com/github/bot/curiosone/app/chat/chatObjs/SendButton.java
@@ -1,33 +1,42 @@
 package com.github.bot.curiosone.app.chat.chatObjs;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.github.bot.curiosone.app.chat.world.AITemporanea;
+import com.github.bot.curiosone.app.chat.world.ChatWorld;
 
 
 public class SendButton extends TextButton {
 
-  private Inserimento inserimento;
-  private Table table;
+  private ChatWorld world;
   private Skin skin = new Skin(Gdx.files.internal("chat-asset/uiskin.json"));  // Da mettere in AssetLoader
+  private AITemporanea ai = new AITemporanea();
 
-  public SendButton(Inserimento inserimento, float width, float height, float x, float y, Table table) {
+  public SendButton(float width, float height, float x, float y) {
     super("send", new Skin(Gdx.files.internal("chat-asset/uiskin.json")));
     this.setPosition(x, y);
     this.setSize(width, height);
-    this.inserimento = inserimento;
-    this.table = table;
     this.addListener(this.click());
   }
 
   private void onClick() {
+    Table table = world.getScrollTable();
+    Inserimento inserimento = world.getInserimento();
+    ScrollPane scrollPane = world.getScrollpane();
     table.row();
     table.add(new Label(inserimento.getText(), skin)).expandX().right();
+    table.row();
+    table.add(new Label(ai.getRisposta(inserimento.getText()), skin)).expandX().left();
+    scrollPane.scrollTo(0, 0, scrollPane.getWidth(), scrollPane.getHeight());
     table.bottom();
+
     inserimento.setText("");
     this.addListener(this.click());
   }
@@ -39,5 +48,9 @@ public class SendButton extends TextButton {
         onClick();
       }
     };
+  }
+
+  public void setWorld(ChatWorld world) {
+    this.world = world;
   }
 }
