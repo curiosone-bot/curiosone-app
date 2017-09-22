6b9699e2435a4c74f91993f0c52c8ec439c908a0
Emanuele Volanti <emavolant@gmail.com>
2017-09-22 22:54:03 +0200

    Change background  color

--- core/src/com/github/bot/curiosone/app/chat/helpers/AbstractScreen.java
+++ core/src/com/github/bot/curiosone/app/chat/helpers/AbstractScreen.java
@@ -16,7 +16,7 @@ public abstract class AbstractScreen extends Stage implements Screen {
 
   @Override
   public void render(float delta) {
-    Gdx.gl.glClearColor(1, 0, 0, 1);
+    Gdx.gl.glClearColor(1, 1, 1, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
     super.act(delta);
