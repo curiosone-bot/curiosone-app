7e40caabcee44c51de43c5a9346a42a8e4997a95
Thrasher96 <simmys96@gmail.com>
2018-03-16 15:18:17 +0100

    Code updated

--- core/src/com/arkanoid/WorldController.java
+++ core/src/com/arkanoid/WorldController.java
@@ -20,30 +20,26 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
  * @author Simone
  *
  */
-public class WorldController
-{
+public class WorldController {
 
-	private Game game;
+    private Game game;
 
-	private Level level;
+    private Level level;
+    private Ball ball;
+    private Paddle paddle;
 
-	private Ball ball;
-	private Paddle paddle;
+    private int blocksLeft;
+    private int lives;
+    private int score;
+    private int currentLevel;
 
-	private int blocksLeft;
+    private Stage stage;
 
-	private int lives;
-	private int score;
-
-	private int currentLevel;
-
-	private Stage stage;
-
-	private Sound sound;
+    private Sound sound;
 
     private GameInputProcessor processor;
 
-	public WorldController(Game game, int startLevel) throws IllegalFormatException {
+    public WorldController(Game game, int startLevel) throws IllegalFormatException {
         this.game = game;
         currentLevel = startLevel;
         sound = Resources.getInstance().getSound();
@@ -51,9 +47,9 @@ public class WorldController
         stage = new Stage();
         processor = new GameInputProcessor(this);
         init();
-	}
+    }
 
-	private void init() throws IllegalFormatException {
+    private void init() throws IllegalFormatException {
         level = new Level(currentLevel);
         paddle = new Paddle();
         ball = new Ball();
@@ -62,22 +58,30 @@ public class WorldController
 
         stage.addActor(paddle);
         stage.addActor(ball);
-        processor.setWorld(this);//updated
-	}
+        processor.setWorld(this);
+    }
 
-	public void update(float deltaTime) throws IllegalFormatException {
+    /**
+     * updates the game logic
+     *
+     * @param deltaTime the time interval between each update
+     * @throws IllegalFormatException
+     */
+    public void update(float deltaTime) throws IllegalFormatException {
         stage.act(deltaTime);
 
         //updates every actor
         paddle.updateMotion(deltaTime);
-        if (!ball.getMotion())
+        if (!ball.getMotion()) {
             ball.translate(paddle);
+        }
         ball.updateMotion(deltaTime);
-		if (ball.getY() + ball.getRadius() < 0) 
-			loseLive();
+        if (ball.getY() + ball.getRadius() < 0) {
+            loseLive();
+        }
 
-		//Check if the ball hits a block
-		for (Block[] line : level.getBlocks()) {
+        //Check if the ball hits a block
+        for (Block[] line : level.getBlocks()) {
             for (Block b : line) {
                 if (b != null && !b.isDestroyed()) {
                     if (Intersector.overlaps(ball.getBounds(), b.getBounds())) {
@@ -91,78 +95,83 @@ public class WorldController
                     }
                 }
             }
-		}
+        }
 
-		//check if the ball hits the paddle
+        //check if the ball hits the paddle
         if (ball.getMotion() && Intersector.overlaps(ball.getBounds(), paddle.getBounds()))
             ball.bounce(paddle);
 
-		//when all destructible blocks are took down, go to the next level
-		if (blocksLeft == 0) nextLevel();
-	}
+        //when all destructible block are taken down go to the next level
+        if (blocksLeft == 0) nextLevel();
+    }
 
     /**
-     * Goes to the next level when the current level is cleared
+     * Goes to the next level if the current level is cleared if it's not the last level
+     * otherwise set the {@link GameState} to {@link GameState#GAME_OVER}
+     *
      * @throws IllegalFormatException
      */
-	private void nextLevel() throws IllegalFormatException {
-		if (currentLevel < 10) {
+    private void nextLevel() throws IllegalFormatException {
+        if (currentLevel < 10) {
             currentLevel++;
             addLive();
             init();
-		}
-		else {
+        } else {
             ((GameScreen) game.getScreen()).setState(GameState.GAME_OVER);
-		}
-	}
+        }
+    }
 
     /**
      * Loses a live and reset ball and paddle position and movement.
      * If there are not lives left set the {@link GameState} to {@link GameState#GAME_OVER}
      */
-	private void loseLive()
-	{
+    private void loseLive() {
         lives--;
         if (lives == 0) {
-            ((GameScreen) game.getScreen()).setState(GameState.GAME_OVER);
-		}
-		else {
+            ((GameScreen)game.getScreen()).setState(GameState.GAME_OVER);
+        }
+        else {
             paddle = new Paddle();
             ball.setMotion(false);
             ball.setPosition(paddle);
             processor.setWorld(this);
-		}
-	}
-	public void addLive() {
+        }
+    }
+
+    public void addLive() {
         lives++;
-	}
+    }
 
-	public int getLives() {
+    public int getLives() {
         return lives;
-	}
+    }
 
-	public Level getLevel() {
+    public Level getLevel() {
         return level;
-	}
-	public Paddle getPaddle() {
+    }
+
+    public Paddle getPaddle() {
         return paddle;
-	}
-	public Ball getBall() {
+    }
+
+    public Ball getBall() {
         return ball;
-	}
+    }
 
-	public Stage getStage() {
+    public Stage getStage() {
         return stage;
-	}
-	public int getScore() {
+    }
+
+    public int getScore() {
         return score;
-	}
+    }
 
-	public Game getGame() {
+    public Game getGame() {
         return game;
-	}
+    }
 
-	public GameInputProcessor getInputProcessor() {
+    public GameInputProcessor getInputProcessor() {
         return processor;
     }
+
 }
